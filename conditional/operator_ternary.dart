33fc8e55b48164d9f6dc149264ec0e51e2d0ea77
Adya <adyka013@gmail.com>
2022-01-22 08:45:26 +0700

    Dasar

---
+++ conditional/operator_ternary.dart
@@ -0,0 +1,5 @@
+viod main(){
+    var isThisWahyu = true;
+
+    isThisWahyu ? print("Wahyu") : print("Bukan");
+}
---
+++ looping/while.dart
@@ -0,0 +1,20 @@
+void main (){
+  var flag = 2;
+  while(flag < 20) { 
+  print (flag.toString() +" - I Love Coding " ); 
+  flag++; 
+  } 
+}
+
+
+// void main() {
+ 
+//   for (var deret = 2; deret <= 20; deret += 2) {
+ 
+//     print( deret.toString() + ' - I Love Coding' );
+ 
+//   }
+ 
+  
+ 
+// }
